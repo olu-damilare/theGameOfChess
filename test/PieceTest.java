81d20a1087c0db5c5015eb69df07deca462e10a7
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-31 19:41:45 +0100

    test

--- test/PieceTest.java
+++ test/PieceTest.java
@@ -11,25 +11,25 @@ public class PieceTest {
     Position position;
     Piece piece;
 
-    @BeforeEach
-    void setUp(){
-        position = new Position(1,8);
-        piece = new Piece(BLACK, position);
-    }
-
-    @Test
-    void testThatChessPieceHasAColour(){
-        assertEquals(BLACK, piece.getColour());
-    }
-
-    @Test
-    void testThatChessPieceHasAPosition(){
-        assertEquals(new Position(1, 8), piece.getCurrentPosition());
-    }
-    @Test
-    void testThatAChessPieceHasAFloorItOccupies(){
-        Floor floor = new Floor(8, 'a');
-        assertEquals(floor, piece.getCurrentFloor());
-    }
+//    @BeforeEach
+//    void setUp(){
+//        position = new Position(1,8);
+//        piece = new Piece(BLACK, position);
+//    }
+//
+//    @Test
+//    void testThatChessPieceHasAColour(){
+//        assertEquals(BLACK, piece.getColour());
+//    }
+//
+//    @Test
+//    void testThatChessPieceHasAPosition(){
+//        assertEquals(new Position(1, 8), piece.getCurrentPosition());
+//    }
+//    @Test
+//    void testThatAChessPieceHasAFloorItOccupies(){
+//        Floor floor = new Floor(8, 'a');
+//        assertEquals(floor, piece.getCurrentFloor());
+//    }
 
 }
