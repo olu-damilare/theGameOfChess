1136abf1e60ef5e3aaf3a74ab3fe2142b33c0fb9
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-02 17:25:22 +0100

    added features

---
+++ src/gameExceptions/CapturedPieceQueryException.java
@@ -0,0 +1,7 @@
+package gameExceptions;
+
+public class CapturedPieceQueryException extends ChessGameExceptions{
+    public CapturedPieceQueryException(String message) {
+        super(message);
+    }
+}
