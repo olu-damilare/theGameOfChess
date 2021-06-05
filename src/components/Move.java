b221d3315e3a690665255e0e1b1f2d3e3de5bbd6
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-05 22:52:17 +0100

    added features

--- src/components/Move.java
+++ src/components/Move.java
@@ -1,4 +1,27 @@
 package components;
 
+import java.util.Objects;
+
 public class Move {
+    private final Floor previousFloor;
+    private final Floor currentFloor;
+
+    public Move(Floor previousFloor, Floor currentFloor) {
+        this.previousFloor = previousFloor;
+        this.currentFloor = currentFloor;
+    }
+
+    @Override
+    public String toString() {
+        return previousFloor + " " + currentFloor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Move move = (Move) o;
+        return Objects.equals(previousFloor, move.previousFloor) && Objects.equals(currentFloor, move.currentFloor);
+    }
+
+
 }
