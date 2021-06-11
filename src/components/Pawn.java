34e95ad70fdeb109ce5c81b5fda88b897d948561
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-11 18:03:06 +0100

    added features

--- src/components/Pawn.java
+++ src/components/Pawn.java
@@ -16,11 +16,51 @@ public class Pawn extends Piece {
 
     @Override
     public void move(Floor destinationFloor, Board board) {
+        boolean oneStepMove = destinationFloor.getRank() - getCurrentFloor().getRank() == 1;
+        boolean twoStepMove = destinationFloor.getRank() - getCurrentFloor().getRank() == 2;
+        Piece enemy;
+        boolean validCaptureMove = false;
+        boolean matchingColours;
+
+        if(destinationFloor.getCurrentOccupant() != null) {
+            enemy = destinationFloor.getCurrentOccupant();
+            matchingColours = enemy.getColour() == getColour();
+            validCaptureMove = oneStepMove && (Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank()) == 1) && !matchingColours;
+        }
+
+        boolean validMove = (hasMadeFirstMove && oneStepMove) || (!hasMadeFirstMove && (oneStepMove || twoStepMove));
 
 
+        if(!validMove ){
+            throw new InvalidMoveException("Invalid move");
+        }
+        if(validCaptureMove){
+            throw new InvalidMoveException("Invalid move");
+        }
+
+
+
+        if(!hasMadeFirstMove) {
+                updateFloorsStatus(destinationFloor);
+                hasMadeFirstMove = true;
+        }else{
+            updateFloorsStatus(destinationFloor);
+        }
+
+    }
+
+    private void updateFloorsStatus(Floor destinationFloor) {
+        getCurrentFloor().setOccupant(null);
+        getCurrentFloor().setOccupyStatus(false);
+        assignFloor(destinationFloor);
     }
 
     public boolean hasMadeFirstMove(){
         return hasMadeFirstMove;
+    }
+
+    @Override
+    public String toString() {
+        return "Pawn\n" + super.toString();
     }
 }
