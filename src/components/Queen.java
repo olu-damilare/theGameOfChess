cbd240384c022f9c383ca4d5bcb76032a6d96001
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-16 12:29:24 +0100

    added features

--- src/components/Queen.java
+++ src/components/Queen.java
@@ -13,10 +13,95 @@ public class Queen extends Piece{
 
     @Override
     public void move(Floor destinationFloor, Board board) {
+        validateDirection(destinationFloor);
+        Bishop bishop  = new Bishop(Colour.BLACK, getCurrentFloor());
+        Rook rook = new Rook(Colour.BLACK, getCurrentFloor());
 
+        boolean isUpperLeft = isUpperLeftDirection(destinationFloor);
+        boolean isUpperRight = isUpperRightDirection(destinationFloor);
+        boolean isLowerLeft = isLowerLeftDirection(destinationFloor);
+        boolean isLowerRight = isLowerRightDirection(destinationFloor);
+        boolean isNorthDirection = isNorthDirection(destinationFloor);
+        boolean isSouthDirection = isSouthDirection(destinationFloor);
+        boolean isEastDirection = isEastDirection(destinationFloor);
+        boolean isWestDirection = isWestDirection(destinationFloor);
 
-        getCurrentFloor().setOccupant(null);
-        getCurrentFloor().setOccupyStatus(false);
-        assignFloor(destinationFloor);
+        validateForMoveObstruction(destinationFloor, board, bishop, rook, isUpperLeft, isUpperRight, isLowerLeft,
+                isLowerRight, isNorthDirection, isSouthDirection, isEastDirection, isWestDirection);
+
+        if(destinationFloor.isOccupied()){
+            if(destinationFloor.getCurrentOccupant().getColour() == getColour()){
+                throw new InvalidMoveException("Invalid move");
+            }
+            capture(destinationFloor.getCurrentOccupant());
+        }
+        updateFloorsStatus(destinationFloor);
+
     }
+
+    private void validateForMoveObstruction(Floor destinationFloor, Board board, Bishop bishop, Rook rook, boolean isUpperLeft, boolean isUpperRight, boolean isLowerLeft, boolean isLowerRight, boolean isNorthDirection, boolean isSouthDirection, boolean isEastDirection, boolean isWestDirection) {
+        if(isUpperLeft){
+            bishop.checkForObstructionInUpperLeft(destinationFloor, board);
+        }else if(isUpperRight){
+            bishop.checkForObstructionInUpperRight(destinationFloor, board);
+        }else if(isLowerRight){
+            bishop.checkForObstructionInLowerRight(destinationFloor, board);
+        }else if(isLowerLeft){
+            bishop.checkForObstructionInLowerLeft(destinationFloor, board);
+        }else {
+            rook.validateForMoveObstruction(destinationFloor, board, isNorthDirection, isSouthDirection, isEastDirection, isWestDirection);
+        }
+    }
+
+
+
+    private void validateDirection(Floor destinationFloor) {
+        int rankDifference = Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank());
+        int fileDifference = Math.abs(destinationFloor.getFile() - getCurrentFloor().getFile());
+        boolean isDiagonalMove = fileDifference == rankDifference;
+        boolean isVerticalMove = (Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank()) > 0) &&
+                destinationFloor.getFile() == getCurrentFloor().getFile();
+        boolean isHorizontalMove = (Math.abs(destinationFloor.getFile() - getCurrentFloor().getFile()) > 0) &&
+                destinationFloor.getRank() == getCurrentFloor().getRank();
+        if(!isDiagonalMove && !isVerticalMove && !isHorizontalMove)
+            throw new InvalidMoveException("Invalid move");
+    }
+
+    private boolean isLowerRightDirection(Floor destinationFloor) {
+        return destinationFloor.getRank() < getCurrentFloor().getRank() &&
+                destinationFloor.getFile() > getCurrentFloor().getFile();
+    }
+
+    private boolean isLowerLeftDirection(Floor destinationFloor) {
+        return destinationFloor.getRank() < getCurrentFloor().getRank() &&
+                destinationFloor.getFile() < getCurrentFloor().getFile();
+    }
+
+    private boolean isUpperRightDirection(Floor destinationFloor) {
+        return destinationFloor.getRank() > getCurrentFloor().getRank() &&
+                destinationFloor.getFile() > getCurrentFloor().getFile();
+    }
+
+    private boolean isUpperLeftDirection(Floor destinationFloor) {
+        return destinationFloor.getRank() > getCurrentFloor().getRank() &&
+                destinationFloor.getFile() < getCurrentFloor().getFile();
+    }
+
+    private boolean isWestDirection(Floor destinationFloor) {
+        return getCurrentFloor().getFile() - destinationFloor.getFile() > 0;
+    }
+
+    private boolean isEastDirection(Floor destinationFloor) {
+        return destinationFloor.getFile() - getCurrentFloor().getFile() > 0;
+    }
+
+    private boolean isSouthDirection(Floor destinationFloor) {
+        return getCurrentFloor().getRank() - destinationFloor.getRank() > 0;
+    }
+
+    private boolean isNorthDirection(Floor destinationFloor) {
+        return destinationFloor.getRank() - getCurrentFloor().getRank() > 0;
+    }
+
+
 }
