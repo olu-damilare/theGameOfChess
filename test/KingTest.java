b22b8e1ab3b2a00bb36a47560f65b601e3e142ad
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-18 15:26:07 +0100

    test

---
+++ test/KingTest.java
@@ -0,0 +1,163 @@
+import components.Board;
+import components.Floor;
+import components.King;
+import gameExceptions.InvalidMoveException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static components.Colour.BLACK;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KingTest {
+
+    Board board;
+
+    @BeforeEach
+    void setUp(){
+        board = new Board(8,8);
+    }
+
+    @AfterEach
+    void tearDown(){
+        board = null;
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepForward(){
+        Floor floor = board.getFloor(1,4);
+        Floor destinationFloor = board.getFloor(2,4);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepBackward(){
+        Floor floor = board.getFloor(2,4);
+        Floor destinationFloor = board.getFloor(1,4);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepEastward(){
+        Floor floor = board.getFloor(1,4);
+        Floor destinationFloor = board.getFloor(1,5);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepWestward(){
+        Floor floor = board.getFloor(1,4);
+        Floor destinationFloor = board.getFloor(1,3);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepDiagonallyInUpperLeftDirection(){
+        Floor floor = board.getFloor(1,4);
+        Floor destinationFloor = board.getFloor(2,3);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepDiagonallyInUpperRightDirection(){
+        Floor floor = board.getFloor(1,4);
+        Floor destinationFloor = board.getFloor(2,5);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepDiagonallyInLowerLeftDirection(){
+        Floor floor = board.getFloor(2,4);
+        Floor destinationFloor = board.getFloor(1,3);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingCanMoveOneStepDiagonallyInLowerRightDirection(){
+        Floor floor = board.getFloor(2,4);
+        Floor destinationFloor = board.getFloor(1,5);
+        King king = new King(BLACK, floor);
+        assertEquals(floor, king.getCurrentFloor());
+        assertEquals(king, floor.getCurrentOccupant());
+
+        king.move(destinationFloor, board);
+        assertEquals(destinationFloor, king.getCurrentFloor());
+        assertEquals(king, destinationFloor.getCurrentOccupant());
+        assertNull(floor.getCurrentOccupant());
+    }
+
+    @Test
+    void testThatKingNotMovingOneStep_throwsInvalidMoveException(){
+        Floor floor = board.getFloor(4,4);
+        King king = new King(BLACK, floor);
+
+        Floor firstFloor = board.getFloor(6,2);
+        Floor secondFloor = board.getFloor(2,4);
+        Floor thirdFloor = board.getFloor(4,6);
+        Floor fourthFloor = board.getFloor(2,4);
+        Floor fifthFloor = board.getFloor(6,6);
+        Floor sixthFloor = board.getFloor(2,6);
+        Floor seventhFloor = board.getFloor(2,2);
+        Floor eighthFloor = board.getFloor(2,2);
+
+        assertThrows(InvalidMoveException.class, ()-> king.move(firstFloor, board));
+        assertThrows(InvalidMoveException.class, ()-> king.move(secondFloor, board));
+        assertThrows(InvalidMoveException.class, ()-> king.move(thirdFloor, board));
+        assertThrows(InvalidMoveException.class, ()-> king.move(fourthFloor, board));
+        assertThrows(InvalidMoveException.class, ()-> king.move(fifthFloor, board));
+        assertThrows(InvalidMoveException.class, ()-> king.move(sixthFloor, board));
+        assertThrows(InvalidMoveException.class, ()-> king.move(seventhFloor, board));
+        assertThrows(InvalidMoveException.class, ()-> king.move(eighthFloor, board));
+
+
+    }
+}
