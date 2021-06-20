63f1f2aa555fba2ee53678e6e2d0aedcb18b62ad
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-20 01:23:43 +0100

    added features

--- src/components/King.java
+++ src/components/King.java
@@ -5,24 +5,42 @@ import gameExceptions.InvalidMoveException;
 public class King extends Piece {
 
 
+    private boolean isChecked;
+
     public King(Colour colour, Floor defaultFloor) {
         super(colour, defaultFloor);
     }
 
     @Override
     public void move(Floor destinationFloor, Board board) {
-       boolean northAndSouth =  Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank()) == 1 &&
-               (destinationFloor.getFile() - getCurrentFloor().getFile() == 0);
+        validateMove(destinationFloor);
+
+        if(destinationFloor.isOccupied()){
+            if(destinationFloor.getCurrentOccupant().getColour() == getColour()){
+                throw new InvalidMoveException("Invalid move.");
+            }
+            capture(destinationFloor.getCurrentOccupant());
+
+        }
+
+        updateFloorsStatus(destinationFloor);
+    }
+
+    private void validateMove(Floor destinationFloor) {
+        boolean northAndSouth =  Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank()) == 1 &&
+                (destinationFloor.getFile() - getCurrentFloor().getFile() == 0);
         boolean eastAndWest =  Math.abs(destinationFloor.getFile() - getCurrentFloor().getFile()) == 1 &&
                 (destinationFloor.getRank() - getCurrentFloor().getRank() == 0);
 
         int rankDifference = Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank());
         int fileDifference = Math.abs(destinationFloor.getFile() - getCurrentFloor().getFile());
         boolean isDiagonalMove = (fileDifference == 1) && (rankDifference == 1);
+
         if(!isDiagonalMove && !northAndSouth && !eastAndWest)
             throw new InvalidMoveException("Invalid move");
-
-
-        updateFloorsStatus(destinationFloor);
+    }
+    
+    public void setChecked(boolean checked){
+        isChecked = checked;
     }
 }
