cdc7538dbf68b5af1fe265fa8ad75412547265e0
Damilare Jolayemi <djolayemi@gmail.com>
2021-06-28 14:32:46 +0100

    added features

--- src/components/King.java
+++ src/components/King.java
@@ -7,6 +7,7 @@ public class King extends Piece {
 
     private boolean isChecked;
     private boolean hasMadeFirstMove;
+    private boolean hasCastled;
 
     public King(Colour colour, Floor defaultFloor) {
         super(colour, defaultFloor);
@@ -16,8 +17,8 @@ public class King extends Piece {
     public void move(Floor destinationFloor, Board board) {
         validateMove(destinationFloor);
 
-        if(destinationFloor.isOccupied()){
-            if(destinationFloor.getCurrentOccupant().getColour() == getColour()){
+        if (destinationFloor.isOccupied()) {
+            if (destinationFloor.getCurrentOccupant().getColour() == getColour()) {
                 throw new InvalidMoveException("Invalid move.");
             }
             capture(destinationFloor.getCurrentOccupant());
@@ -30,28 +31,78 @@ public class King extends Piece {
     }
 
     private void validateMove(Floor destinationFloor) {
-        boolean northAndSouth =  Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank()) == 1 &&
+        boolean northAndSouth = Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank()) == 1 &&
                 (destinationFloor.getFile() - getCurrentFloor().getFile() == 0);
-        boolean eastAndWest =  Math.abs(destinationFloor.getFile() - getCurrentFloor().getFile()) == 1 &&
+        boolean eastAndWest = Math.abs(destinationFloor.getFile() - getCurrentFloor().getFile()) == 1 &&
                 (destinationFloor.getRank() - getCurrentFloor().getRank() == 0);
 
         int rankDifference = Math.abs(destinationFloor.getRank() - getCurrentFloor().getRank());
         int fileDifference = Math.abs(destinationFloor.getFile() - getCurrentFloor().getFile());
         boolean isDiagonalMove = (fileDifference == 1) && (rankDifference == 1);
 
-        if(!isDiagonalMove && !northAndSouth && !eastAndWest)
+        if (!isDiagonalMove && !northAndSouth && !eastAndWest)
             throw new InvalidMoveException("Invalid move");
     }
-    
-    public void setChecked(boolean checked){
-        isChecked = checked;
+
+
+    public boolean isChecked() {
+        return isChecked;
     }
 
     public void castle(Rook rook, Board board) {
+        boolean queenSizeCastle = getCurrentFloor().getFile() > rook.getCurrentFloor().getFile();
+        boolean kingSideCastle = getCurrentFloor().getFile() < rook.getCurrentFloor().getFile();
+
+        if (hasMadeFirstMove || rook.hasMadeFirstMove()) {
+            throw new InvalidMoveException("Invalid move");
+        }
+
+
+        if (queenSizeCastle) {
+            Floor floor = board.getFloor(1, 3);
+            assignFloor(floor);
+            int rookFloorFile = 4;
+            rook.castle(rookFloorFile, board);
+        } else if (kingSideCastle) {
+            Floor floor = board.getFloor(1, 7);
+            assignFloor(floor);
+            int rookFloorFile = 6;
+            rook.castle(rookFloorFile, board);
+        }
+        hasMadeFirstMove = true;
+        hasCastled = true;
+
 
     }
 
     public boolean hasMadeFirstMove() {
         return hasMadeFirstMove;
+    }
+
+
+    public boolean hasCastled() {
+        return hasCastled;
+    }
+
+    public void scanForChecked(Board board) {
+        scanForPawnCheck(board);
+    }
+
+    private void scanForPawnCheck(Board board) {
+        Floor upperLeftFloor = board.getFloor(getCurrentFloor().getRank() + 1, getCurrentFloor().getFile() - 1);
+        Floor upperRightFloor = board.getFloor(getCurrentFloor().getRank() + 1, getCurrentFloor().getFile() + 1);
+        if (upperRightFloor.isOccupied()) {
+            boolean upperRightCheck = upperRightFloor.getCurrentOccupant().getColour() != getColour() &&
+                    upperRightFloor.getCurrentOccupant().getClass().toString().equals("class components.Pawn");
+            if (upperRightCheck) {
+                isChecked = true;
+            }
+        } else if (upperLeftFloor.isOccupied()) {
+            boolean upperLeftCheck = upperLeftFloor.getCurrentOccupant().getColour() != getColour() &&
+                    upperLeftFloor.getCurrentOccupant().getClass().toString().equals("components.Pawn");
+            if (upperLeftCheck) {
+                isChecked = true;
+            }
+        }
     }
 }
