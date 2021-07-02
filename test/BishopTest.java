f6b1195565b6bda84473814031aef769d8f0845b
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-02 11:35:09 +0100

    'test

--- test/BishopTest.java
+++ test/BishopTest.java
@@ -4,7 +4,7 @@ import game.pieces.Bishop;
 import game.pieces.Pawn;
 import game.pieces.Piece;
 import game.properties.Move;
-import gameExceptions.InvalidMoveException;
+import game.gameExceptions.InvalidMoveException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
