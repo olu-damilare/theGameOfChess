b0fd360a94ec47b0df432e7da3a44485788d07fa
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-02 11:18:48 +0100

    added features

--- src/components/King.java
+++ src/components/King.java
@@ -4,10 +4,10 @@ import gameExceptions.InvalidMoveException;
 
 public class King extends Piece {
 
-
     private boolean isChecked;
     private boolean hasMadeFirstMove;
     private boolean hasCastled;
+    private KingChecker observer = new KingChecker();
 
     public King(Colour colour, Floor defaultFloor) {
         super(colour, defaultFloor);
@@ -85,108 +85,14 @@ public class King extends Piece {
     }
 
     public void scanForChecked(Board board) {
-        scanForPawnCheck(board);
-        scanForBishopCheck(board);
+        observer.scanForPawnCheck(board, this);
+        observer.scanForBishopCheck(board, this);
+        observer.scanForKnightCheck(board, this);
+
+
     }
 
-    private void scanForPawnCheck(Board board) {
-        Floor upperLeftFloor = board.getFloor(getCurrentFloor().getRank() + 1, getCurrentFloor().getFile() - 1);
-        Floor upperRightFloor = board.getFloor(getCurrentFloor().getRank() + 1, getCurrentFloor().getFile() + 1);
-        if (upperRightFloor.isOccupied()) {
-            boolean upperRightCheck = upperRightFloor.getCurrentOccupant().getColour() != getColour() &&
-                    upperRightFloor.getCurrentOccupant().getClass().toString().equals("class components.Pawn");
-            if (upperRightCheck) {
-                isChecked = true;
-            }
-        } else if (upperLeftFloor.isOccupied()) {
-            boolean upperLeftCheck = upperLeftFloor.getCurrentOccupant().getColour() != getColour() &&
-                    upperLeftFloor.getCurrentOccupant().getClass().toString().equals("components.Pawn");
-            if (upperLeftCheck) {
-                isChecked = true;
-            }
-        }
-    }
-
-    private void scanForBishopCheckInUpperLeft(Board board){
-        int rankCounter = getCurrentFloor().getRank() + 1;
-        int fileCounter = getCurrentFloor().getFile() - 1;
-
-        while(fileCounter >= 1){
-            Floor floor = board.getFloor(rankCounter, fileCounter);
-            if(floor.isOccupied()){
-                if(floor.getCurrentOccupant().getColour() == getColour()) {
-                    break;
-                }else if(floor.getCurrentOccupant().getClass().toString().equals("class components.Bishop")){
-                    isChecked = true;
-                    break;
-                }
-            }
-            rankCounter++;
-            fileCounter--;
-        }
-    }
-
-    private void scanForBishopCheckInUpperRight(Board board){
-        int rankCounter = getCurrentFloor().getRank() + 1;
-        int fileCounter = getCurrentFloor().getFile() + 1;
-
-        while(fileCounter <= 8){
-            Floor floor = board.getFloor(rankCounter, fileCounter);
-            if(floor.isOccupied()){
-                if(floor.getCurrentOccupant().getColour() == getColour()) {
-                    break;
-                }else if(floor.getCurrentOccupant().getClass().toString().equals("class components.Bishop")){
-                    isChecked = true;
-                    break;
-                }
-            }
-            rankCounter++;
-            fileCounter++;
-        }
-    }
-
-    private void scanForBishopCheckInLowerRight(Board board){
-        int rankCounter = getCurrentFloor().getRank() - 1;
-        int fileCounter = getCurrentFloor().getFile() + 1;
-
-        while(rankCounter >= 1){
-            Floor floor = board.getFloor(rankCounter, fileCounter);
-            if(floor.isOccupied()){
-                if(floor.getCurrentOccupant().getColour() == getColour()) {
-                    break;
-                }else if(floor.getCurrentOccupant().getClass().toString().equals("class components.Bishop")){
-                    isChecked = true;
-                    break;
-                }
-            }
-            rankCounter--;
-            fileCounter++;
-        }
-    }
-
-    private void scanForBishopCheckInLowerLeft(Board board){
-        int rankCounter = getCurrentFloor().getRank() - 1;
-        int fileCounter = getCurrentFloor().getFile() - 1;
-
-        while(rankCounter >= 1){
-            Floor floor = board.getFloor(rankCounter, fileCounter);
-            if(floor.isOccupied()){
-                if(floor.getCurrentOccupant().getColour() == getColour()) {
-                    break;
-                }else if(floor.getCurrentOccupant().getClass().toString().equals("class components.Bishop")){
-                    isChecked = true;
-                    break;
-                }
-            }
-            rankCounter--;
-            fileCounter--;
-        }
-    }
-
-    private void scanForBishopCheck(Board board){
-        scanForBishopCheckInUpperLeft(board);
-        scanForBishopCheckInUpperRight(board);
-        scanForBishopCheckInLowerRight(board);
-        scanForBishopCheckInLowerLeft(board);
+    public void setChecked(boolean isChecked) {
+        this.isChecked = isChecked;
     }
 }
